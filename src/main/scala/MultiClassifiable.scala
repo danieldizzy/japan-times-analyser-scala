cfa292ed0a6e81e792cc27621c0fc68b30eb4dc8
Ryo Ota <Jimmy@Ryo-no-MacBook-Pro.local>
2016-12-07 22:44:34 +0900

    [add] create only class of MultiDataset and MultiClassifiable, I will create these sub classes later

---
+++ src/main/scala/MultiClassifiable.scala
@@ -0,0 +1,6 @@
+/**
+  * Created by Jimmy on 5/12/2016.
+  */
+trait MultiClassifiable {
+  def multiDataset(): MultiDataset
+}
---
+++ src/main/scala/MultiDataset.scala
@@ -0,0 +1,9 @@
+/**
+  * Created by Ryo on 2016/11/25.
+  */
+/**
+  * Data structure for Binary Classification
+  *
+  * @param docsSeq
+  */
+case class MultiDataset(docsSeq: Seq[Seq[EngDocument]])
