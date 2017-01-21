fd61845f9cedb4482597de53cbbba118c69d8048
Ryo Ota <Jimmy@Ryo-no-MacBook-Pro.local>
2017-01-21 10:38:14 +0900

    [refactor] Change commentouts to “if(false)” to take the power of compiler.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -7,6 +7,7 @@ object Main {
 
   /**
     * A sample of [[EngDocument]]s
+    *
     * @return
     */
   private[this] def sampleDocs(): Seq[EngDocument] = {
@@ -24,6 +25,7 @@ object Main {
 
   /**
     * A [[EngDocument]]s from the file
+    *
     * @return
     */
   private[this] def docsFromFile(): Seq[EngDocument] = {
@@ -32,62 +34,83 @@ object Main {
   }
 
   def main(args: Array[String]) {
-//
-//    // make train-set file and test-set file for SVM light
-//    TrainAndTestFilesGenerator.generateSvmLightFormatFiles(
-//      FigureAndSumoDataset,
-//      trainFilePath = "./data/train-set",
-//      testFilePath  = "./data/test-set",
-//      trainSetRate = 0.8)
+
+    if(false) {
+      // make train-set file and test-set file for SVM light
+      TrainAndTestFilesGenerator.generateSvmLightFormatFiles(
+        FigureAndSumoDataset,
+        trainFilePath = "./data/train-set",
+        testFilePath = "./data/test-set",
+        trainSetRate = 0.8
+      )
+    }
 
 
-//    TrainAndTestFilesGenerator.generateMultiSvmLightFormatFiles(
-//      JapanTimesDataset,
-//      trainFilePath = "./data/multi-class-train-set",
-//      testFilePath  = "./data/multi-class-test-set",
-//      trainSetRate = 0.8
-//    )
-
-//    val MultiDataset(seq) = JapanTimesDonwloader.onlyTitleMultiClassifiable.multiDataset()
-//    val lengths = seq.flatMap(_.map(_.entity.length))
-//    val average = lengths.sum.toDouble / lengths.length
-//    println("Average length of titles " + average) // 55.72
+    if(false) {
+      TrainAndTestFilesGenerator.generateMultiSvmLightFormatFiles(
+        JapanTimesDataset,
+        trainFilePath = "./data/multi-class-train-set",
+        testFilePath = "./data/multi-class-test-set",
+        trainSetRate = 0.8
+      )
+    }
 
 
-    // Train-Set: only title, Test-Set: only title
-//    TrainAndTestFilesGenerator.generateMultiSvmLightFormatFiles(
-//      JapanTimesDonwloader.onlyTitleMultiClassifiable,
-//      trainFilePath = "./data/multi-class-only-title-train-set",
-//      testFilePath  = "./data/multi-class-only-title-test-set",
-//      trainSetRate = 0.8
-//    )
+    if(false) {
+      // Avevate length of all titles
+      val MultiDataset(seq) = JapanTimesDonwloader.onlyTitleMultiClassifiable.multiDataset()
+      val lengths = seq.flatMap(_.map(_.entity.length))
+      val average = lengths.sum.toDouble / lengths.length
+      println("Average length of titles " + average) // 55.72
+    }
+
+
+    if(false) {
+      // Train-Set: only title, Test-Set: only title
+      TrainAndTestFilesGenerator.generateMultiSvmLightFormatFiles(
+        JapanTimesDonwloader.onlyTitleMultiClassifiable,
+        trainFilePath = "./data/multi-class-only-title-train-set",
+        testFilePath = "./data/multi-class-only-title-test-set",
+        trainSetRate = 0.8
+      )
+    }
 
 
 
-    // Train-Set: title+article, Test-Set: only title
-//    TrainAndTestFilesGenerator.generateTrainTestMultiSvmLightFormatFiles(
-//      JapanTimesDonwloader.`Multi-Classfiable of (train-set: title + article, test-set: only title)`,
-//      trainFilePath = "./data/multi-class-title-article-train-set",
-//      testFilePath  = "./data/multi-class-title-article-test-set",
-//      trainSetRate = 0.8
-//    )
+    if(false) {
+      // Train-Set: title+article, Test-Set: only title
+      TrainAndTestFilesGenerator.generateTrainTestMultiSvmLightFormatFiles(
+        JapanTimesDonwloader.`Multi-Classfiable of (train-set: title + article, test-set: only title)`,
+        trainFilePath = "./data/multi-class-title-article-train-set",
+        testFilePath = "./data/multi-class-title-article-test-set",
+        trainSetRate = 0.8
+      )
+    }
 
 
-//    // Train-Set: many titles+article, Test-Set: only title
-//    TrainAndTestFilesGenerator.generateTrainTestMultiSvmLightFormatFiles(
-//      JapanTimesDonwloader.`Multi-Classfiable of (train-set: many titles + article, test-set: only title)`(titleTimes = 20),
-//      trainFilePath = "./data/multi-class-20-titles-article-train-set",
-//      testFilePath  = "./data/multi-class-20-titles-article-test-set",
-//      trainSetRate = 0.8
-//    )
+    if(false){
+      // Train-Set: many titles+article, Test-Set: only title
+      TrainAndTestFilesGenerator.generateTrainTestMultiSvmLightFormatFiles(
+        JapanTimesDonwloader.`Multi-Classfiable of (train-set: many titles + article, test-set: only title)`(titleTimes = 20),
+        trainFilePath = "./data/multi-class-20-titles-article-train-set",
+        testFilePath = "./data/multi-class-20-titles-article-test-set",
+        trainSetRate = 0.8
+      )
+    }
 
 
-//    TextGeneratorForGensim.generate(dirPath = "./gensim-text")
+    if(false) {
+      // Generate article text from article data
+      TextGeneratorForGensim.generate(dirPath = "./gensim-text")
+    }
 
 
-    // List titles for some articles
-    val artsSeq: Seq[Seq[JapanTimesArticle]] = JapanTimesDonwloader.getJapanTimesArticlesSeq()
-    artsSeq.flatMap(_.take(10)).map(_.title).foreach(println)
+
+    if(true) {
+      // List titles for some articles
+      val artsSeq: Seq[Seq[JapanTimesArticle]] = JapanTimesDonwloader.getJapanTimesArticlesSeq()
+      artsSeq.flatMap(_.take(10)).map(_.title).foreach(println)
+    }
 
   }
 }
